a8b3c89f725fb76c3935963f35a1cc81216f0021
Rafael Laurindo <rafaelfilholm@gmail.com>
2019-04-14 16:39:11 -0300

    feat: add zone to drop and upload files

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import { MdInsertDriveFile } from "react-icons/md";
 import { distanceInWords } from "date-fns";
 import pt from "date-fns/locale/pt";
+import DropZone from "react-dropzone";
 
 import api from "../../services/api";
-
 import logo from "../../assets/logo.png";
 import "./styles.css";
 
@@ -19,6 +19,15 @@ export default class Box extends Component {
     this.setState({ box: response.data });
   }
 
+  handleUpload = files => {
+    files.forEach(async file => {
+      const data = new FormData();
+      data.append("file", file);
+
+      await api.post(`/boxes/${this.state.box._id}/files`, data);
+    });
+  };
+
   render() {
     return (
       <div id="box-container">
@@ -28,10 +37,19 @@ export default class Box extends Component {
           <h2>{this.state.box.title}</h2>
         </header>
 
+        <DropZone onDropAccepted={this.handleUpload}>
+          {({ getRootProps, getInputProps }) => (
+            <div className="upload" {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Arraste arquivos ou clique aqui</p>
+            </div>
+          )}
+        </DropZone>
+
         <ul>
           {this.state.box.files &&
             this.state.box.files.map(file => (
-              <li>
+              <li key={file._id}>
                 <a
                   className="fileInfo"
                   href={file.url}
