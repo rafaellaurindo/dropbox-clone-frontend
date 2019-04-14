d26def39427a9396daa208cef9ccfc07fc04be8e
Rafael Laurindo <rafaelfilholm@gmail.com>
2019-04-14 15:29:29 -0300

    feat: add routes as default component in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import Main from "./pages/Main";
+import Routes from "./routes";
 
-const App = () => <Main />;
+const App = () => <Routes />;
 
 export default App;
