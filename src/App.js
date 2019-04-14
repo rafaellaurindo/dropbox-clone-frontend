9289df6bd6b0c53a739eb5f940a10267b6aff772
Rafael Laurindo <rafaelfilholm@gmail.com>
2019-04-14 15:20:17 -0300

    refactor: rewrite App component stateless format

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import Main from "./pages/Main";
 
-class App extends Component {
-  render() {
-    return <Main />;
-  }
-}
+const App = () => <Main />;
 
 export default App;
