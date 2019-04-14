67833ce48007972458c45ed511ac273b09289db9
Rafael Laurindo <rafaelfilholm@gmail.com>
2019-04-14 13:13:12 -0300

    feat: add main page to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
+import Main from "./pages/Main";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h2>Hello World!!!</h2>
-      </div>
-    );
+    return <Main />;
   }
 }
 
